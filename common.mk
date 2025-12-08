5189c3cd192fe039726d1e34253c93ae818309b2
ShevT <evgeny.shishkov@gmail.com>
2025-12-08 12:19:13 +0300

    Revert "sm8150-common: Switch to NCM USB tethering"
     Fix USB tethering.
     When using NCM, USB tethering stops after a while.
    
    This reverts commit 0de3373275daeaee805aa39f50a3532406e4c430.
    
    Change-Id: Iabef0db4bcf7b78ea6a22987b13e2a0f8d2eeb3c

--- common.mk
+++ common.mk
@@ -263,7 +263,6 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     FrameworksResTarget \
-    NcmTetheringOverlay \
     OPlusFrameworksResCommon \
     OPlusSettingsResCommon \
     OPlusSystemUIResCommon \

f531026bed9d33aa856caea70301028cf51ef2f0
ShevT <evgeny.shishkov@gmail.com>
2026-01-01 21:55:00 +0300

    sm8150-common: Build DolbyAtmos app
    Change-Id: If6434c6baf02a71630ef5351ebb30f972fd9946b

--- common.mk
+++ common.mk
@@ -130,6 +130,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
 
+# Dolby
+PRODUCT_PACKAGES += \
+    DolbyAtmos
+
 # Doze
 PRODUCT_PACKAGES += \
     OplusDoze \

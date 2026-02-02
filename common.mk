3503c89a4cf14fd1aaf22a62d27deb94920e805b
ShevT <evgeny.shishkov@gmail.com>
2026-02-02 13:52:50 +0300

    sm8150-common: up to 8192 dalvik heap
    Change-Id: Ia58d32f7f1992091b5146248c9f3250b297534da

--- common.mk
+++ common.mk
@@ -110,7 +110,7 @@ PRODUCT_PACKAGES += \
     disable_configstore
 
 # Dalvik
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-8192-dalvik-heap.mk)
 
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true

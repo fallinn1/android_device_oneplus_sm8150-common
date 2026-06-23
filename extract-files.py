e6eb101c9e6f8c36c252063bdc59711ee6227b16
ShevT <evgeny.shishkov@gmail.com>
2026-06-23 13:13:18 +0300

    sm8150-common: vendor: Cleanup gatekeeper rc file
     Property vendor.keymaster.strongbox.version does not exist
    
    Change-Id: I4b96cda142a74256d82ecf62365cb7b38c3a2878

--- extract-files.py
+++ extract-files.py
@@ -56,6 +56,8 @@ blob_fixups: blob_fixups_user_type = {
         .patch_file('blob-patches/vendor.sensors.sscrpcd.rc.patch'),
     'vendor/etc/init/vppservice.rc': blob_fixup()
         .patch_file('blob-patches/vppservice.rc.patch'),
+    'vendor/etc/init/android.hardware.gatekeeper@1.0-service-qti.rc.patch': blob_fixup()
+        .patch_file('blob-patches/android.hardware.gatekeeper@1.0-service-qti.rc.patch'),
     ('odm/lib64/mediadrm/libwvdrmengine.so', 'odm/lib64/libwvhidl.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
     ('odm/lib64/libarcsoft_dualcam_refocus_preview.so', 'vendor/lib64/libarcsoft_super_night_raw.so'): blob_fixup()

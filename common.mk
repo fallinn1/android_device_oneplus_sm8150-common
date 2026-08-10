9222deb97d94112daa242bf084b31c7b9cba2e05
Xenoz <musaibpatel03@gmail.com>
2026-08-10 15:53:47 +0530

    Update common.mk

--- common.mk
+++ common.mk
@@ -264,6 +264,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     FrameworksResTarget \
+    OPlusExtrasResCommon \
     OPlusFrameworksResCommon \
     OPlusSettingsResCommon \
     OPlusSystemUIResCommon \
